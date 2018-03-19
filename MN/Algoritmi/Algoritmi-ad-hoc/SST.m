eb05e5ea97b952301cd956a57e2d55941a835f60
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-03-20 00:17:00 +0200

    Add SST and Householder

---
+++ MN/Algoritmi/Algoritmi-ad-hoc/SST.m
@@ -0,0 +1,25 @@
+function [x] = SST(A, b)
+    % if the matrix is not upper triangular, we cannot solve the system
+    % tril(A, -1) gets us what is strictly under the main diagonal
+    % max(matrix) gets us a list of every column's max
+    % max(list) gets us the maximum from the list
+    % [NOTE]: There is a function called "istriu(A)" which returns 1 if A is an
+    % [NOTE]  upper triangular matrix but it doesn't catch the e-16 errors
+    % [NOTE]  which should be ignored
+    if max(max(abs(tril(A, -1)))) > eps
+        disp('The matrix A is not upper triangular!');
+        x = NaN;
+        return;
+    endif
+
+    n = length(b);
+    x = zeros(n, 1);
+
+    x(n) = b(n) / A(n, n);
+
+    % calculate x(i) backwards
+    for i = (n - 1): -1 : 1
+        sum_of_xs = A(i, (i + 1) : n) * x((i + 1) : n, 1);
+        x(i) = (b(i) - sum_of_xs) / A(i, i);
+    endfor
+endfunction
