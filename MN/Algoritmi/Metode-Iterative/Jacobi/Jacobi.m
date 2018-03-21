652859cd5a1d893441ddbf9da1dbb1fc73d7a8d9
Serban Dragos <dragos.marian1998@gmail.com>
2018-03-21 21:55:48 +0200

    Add files via upload

--- MN/Algoritmi/Metode-Iterative/Jacobi/Jacobi.m
+++ MN/Algoritmi/Metode-Iterative/Jacobi/Jacobi.m
@@ -1,34 +1,51 @@
-function [x step]=Jacobi ( A, b ,x0, tol , max_iter)
-    %use this algorithm only if 
-    %max(abs(eig(Ggs))) is < 1
-    % G = N^(-1)*P
-    % N = diag(diag(A))
-    % P = N - A
+function [x step] = Jacobi(A, b, x0, tol, max_iter)
+    % use this algorithm only if it converges
     
-    %for the function to work make sure
-    %that b and x0 are column vectors
+    % sanity checks
+    N = diag(diag(A));
+    P = N - A;
+    Gj = inv(N) * P;
+    % calculate the spectral radius of Ggs
+    sr = max(abs(eig(Gj)));
+    % check if the algorithm converges
     
-    n=length(b); 
+    if  (sr - 1) >= eps
+        x = NaN;
+        step = -1;
+        disp('Matricea nu converge');
+        return;
+    endif
+    
+    % for the function to work make sure
+    % that b and x0 are column vectors
+    
+    n = length(b); 
     x = x0;
+    % iterate to the maximum number of iterations
     for step = 1 : max_iter
+        % iterate through every x(i)
         for i = 1 : n
-            val_sum = A(i,1:n)*x0(1:n);
-            val_x = A(i,i)*x0(i);
-            %val_sum represents the sum of the 
-            %Jacobi algortihm and A(i,i)*x0(i) 
-            %for which we calculate
-            %val_x so that we can 
-            %substract it from val_sum
+            % val_sum represents the sum of the 
+            % Jacobi algortihm and A(i, i) * x0(i) 
+            % for which we calculate
+            % val_x so that we can 
+            % substract it from val_sum
             
-            x(i)  = b(i) - (val_sum - val_x);
-            x(i)  = x(i)/A(i,i);
+            val_sum = A(i, :) * x0(:);
+            val_x = A(i, i) * x0(i);
+            x(i) = b(i) - (val_sum - val_x);
+            x(i) = x(i) / A(i, i);
             
         endfor
-        if norm(x-x0) < tol;
-            %when the values get close enough to
-            %our chosen tolerance we end the function
+        % when the new values get close 
+        % enough to the last values 
+        % regarding the imposed tolerance 
+        % "tol", we reached the solution
+        if norm(x - x0) < tol
             break;
         endif
+        % update the last computed 
+        % values with the new values
         x0 = x;
     endfor
 endfunction 
