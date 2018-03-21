864e715efc5371057f99f022cc02087191ac7cd3
Serban Dragos <dragos.marian1998@gmail.com>
2018-03-21 21:54:49 +0200

    Add files via upload

--- MN/Algoritmi/Metode-Iterative/Gauss-Seidel/GaussSeidel.m
+++ MN/Algoritmi/Metode-Iterative/Gauss-Seidel/GaussSeidel.m
@@ -1,34 +1,49 @@
-function [x step]= GaussSeidel ( A, b, x0, tol, max_iter)
-    %use this algorithm only if 
-    %max(abs(eig(Ggs))) is < 1
-    % G = N^(-1)*P
-    % N = tril(A)
-    % P = N - A
+function [x step] = GaussSeidel(A, b, x0, tol, max_iter)
+    % use this algorithm only if it converges
     
-    %for the function to work make sure
-    %that b and x0 are column vectors
+    % sanity checks
+    N = tril(A);
+    P = N - A;
+    Ggs = inv(N) * P;
+    % calculate the spectral radius of Ggs
+    sr = max(abs(eig(Ggs)));
+    % check if the algorithm converges
     
-    n=length(b);
+    if  (sr - 1) >= eps
+        x = NaN;
+        step = -1;
+        disp('Matricea nu converge');
+        return;
+    endif
+    
+    % for the function to work make sure
+    % that b and x0 are column vectors
+    
+    n = length(b);
     x = x0;
-    % iterate through the maximum number of iterations 
+    % iterate to the maximum number of iterations 
     for step = 1 : max_iter
         % iterate through every x(i)
         for i = 1 : n
-            new_values_sum = A(i,1:i-1)*x(1:i-1);
-            %new_values_sum represents the first sum
-            %of the Gauss-Seidel algorithm
+            % new_values_sum represents the first sum
+            % of the Gauss-Seidel algorithm
+            new_values_sum = A(i, 1 : i - 1) * x(1 : i - 1);
             
-            old_values_sum = A(i,i+1:n)*x0(i+1:n);
-            %old_values_sum represents the second sum
-            %of the Gauss-Seidel algorithm
+            % old_values_sum represents the second sum
+            % of the Gauss-Seidel algorithm
+            old_values_sum = A(i, i + 1 : n) * x0(i + 1 : n);
             
-            x(i)= (b(i)-(old_values_sum+new_values_sum))/A(i,i);
+            x(i) = (b(i) - (old_values_sum + new_values_sum)) / A(i, i);
         endfor
-        if norm(x-x0) < tol
-            %when the values get close enough to
-            %our chosen tolerance we end the function
+        % when the new values get close 
+        % enough to the last values 
+        % regarding the imposed tolerance 
+        % "tol", we reached the solution
+        if norm(x - x0) < tol
             break;
         endif
+        %   update the last computed 
+        %   values with the new values
         x0 = x;
     endfor
 endfunction 
