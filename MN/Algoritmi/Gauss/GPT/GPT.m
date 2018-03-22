09a10d74cf0d2e77a5f2a95125be5e3891baa92d
NăstaseRoxana <37654500+NastaseRoxana@users.noreply.github.com>
2018-03-22 23:14:47 +0200

    Add files via upload

--- MN/Algoritmi/Gauss/GPT/GPT.m
+++ MN/Algoritmi/Gauss/GPT/GPT.m
@@ -1,7 +1,6 @@
 % [USES] Algoritmi-ad-hoc/SST
 function [x] = GPT(A, b)
 	n = size(A)(1);
-
 	for p = 1 : n - 1
 
 		% Initial linia, respectiv coloana corespunzatoare pivotului sunt date de
@@ -11,9 +10,9 @@ function [x] = GPT(A, b)
 
 		% Aflam maximul in modul dintre elementele matricii determinate de pivotul 
 		% initial 
-		[pivot, linie_pivot] = max(abs(A(p : n,p : n)));
+		[pivot, linie_pivot] = max(abs(A(p : n, p : n)));
 		[pivot, coloana_pivot] = max(pivot);
-		linie_pivot = linie_pivot + p -1;
+		linie_pivot = linie_pivot + p - 1;
 		linie_pivot = linie_pivot(coloana_pivot);
 
 		% Interschimbare a liniei initiale a presupusului pivot cu linia elementului
@@ -30,7 +29,7 @@ function [x] = GPT(A, b)
 		% Interschimbare a coloanei initiale a presului pivot cu cea a elementului
 		% ce va fi pivot
 		if p ~= coloana_pivot
-			temp = A(:,p);
+			temp = A(:, p);
 			A(:, p) = A(:, coloana_pivot);
 			A(:, coloana_pivot) = temp;
 		endif
