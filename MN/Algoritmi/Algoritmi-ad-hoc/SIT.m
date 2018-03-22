270323b93090c9dbffbe76e04a1ed6a7d5f73d6b
roxanaN <37616709+roxanaN@users.noreply.github.com>
2018-03-22 22:18:56 +0200

    Add files via upload

---
+++ MN/Algoritmi/Algoritmi-ad-hoc/SIT.m
@@ -0,0 +1,26 @@
+function [x] =  SIT(A, b)
+    % if the matrix is not lower triangular, we cannot solve the system
+    % triu(A, 1) returns the upper triangular portion without the main diagonal
+    % max(matrix) gets us a list of every column's max
+    % max(list) gets us the maximum from the list
+
+    if max(max(abs(triu(A, 1)))) > eps
+    	disp('The matrix A is not lower triangular!');
+        x = NaN;
+        return;
+    endif
+
+	n = length(b);
+	% initialize x with zeros
+	x = zeros(n, 1);
+
+	% calculate x1
+	x(1) = b(1) / A(1, 1);
+
+	% calculate x(i) forwards
+	for i = 2 : n
+		sum_of_xs = A(i, 1 : (i - 1)) * x(1 : (i - 1), 1);
+		x(i) = (b(i) - sum_of_xs) / A(i, i);
+	endfor
+endfunction
+
