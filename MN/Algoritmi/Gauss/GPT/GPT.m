a1628d18c28c90cb5a1ac73c372619f73ec674c5
NăstaseRoxana <37654500+NastaseRoxana@users.noreply.github.com>
2018-03-22 20:53:21 +0200

    Add files via upload

--- MN/Algoritmi/Gauss/GPT/GPT.m
+++ MN/Algoritmi/Gauss/GPT/GPT.m
@@ -1,10 +1,8 @@
 % [USES] Algoritmi-ad-hoc/SST
-function [X] = GPT(A, b)
+function [x] = GPT(A, b)
 	n = size(A)(1);
 
 	for p = 1 : n - 1
-		% Iniatilazam pivotul cu modulul elementului diagonal
-		pivot = abs(A(p,p));
 
 		% Initial linia, respectiv coloana corespunzatoare pivotului sunt date de
 		% pozitia diagonala
@@ -13,14 +11,14 @@ function [X] = GPT(A, b)
 
 		% Aflam maximul in modul dintre elementele matricii determinate de pivotul 
 		% initial 
-		[pivot, linie_pivot] = max(abs(A(p : n, :)));
+		[pivot, linie_pivot] = max(abs(A(p : n,p : n)));
 		[pivot, coloana_pivot] = max(pivot);
 		linie_pivot = linie_pivot + p -1;
 		linie_pivot = linie_pivot(coloana_pivot);
 
 		% Interschimbare a liniei initiale a presupusului pivot cu linia elementului
 		% ce va fi pivot	
-		if p~=linie_pivot
+		if p ~= linie_pivot
 			temp = A(p, :);
 			A(p, :) = A(linie_pivot, :);
 			A(linie_pivot, :) = temp;
@@ -31,7 +29,7 @@ function [X] = GPT(A, b)
 
 		% Interschimbare a coloanei initiale a presului pivot cu cea a elementului
 		% ce va fi pivot
-		if p~=coloana_pivot
+		if p ~= coloana_pivot
 			temp = A(:,p);
 			A(:, p) = A(:, coloana_pivot);
 			A(:, coloana_pivot) = temp;
@@ -39,15 +37,14 @@ function [X] = GPT(A, b)
 
 		% Eliminare Gaussiana
 		for i = p + 1 : n
-			if A(p,p) == 0
-				continue;
-			endif
 			arg = A(i, p) / A(p, p);
 			A(i, :) = A(i, :) - arg * A(p, :);
 			b(i, :) = b(i, :) - arg * b(p, :);
 		endfor
 	endfor
-	X = SST(A, b)
+
+	% Rezolvam sistemul superior triunghiular
+	x = SST(A, b)
 endfunction
 
 
