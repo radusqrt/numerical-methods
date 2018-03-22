1d24c486a669f0e698d337bca9a09961c9fc355a
Serban Dragos <dragos.marian1998@gmail.com>
2018-03-22 21:06:19 +0200

    Add files via upload

--- MN/Algoritmi/Metode-Iterative/Jacobi/Jacobi.m
+++ MN/Algoritmi/Metode-Iterative/Jacobi/Jacobi.m
@@ -7,17 +7,14 @@ function [x step] = Jacobi(A, b, x0, tol, max_iter)
     Gj = inv(N) * P;
     % calculate the spectral radius of Gj
     sr = max(abs(eig(Gj)));
-    % check if the algorithm converges
     
+    % check if the algorithm converges
     if  (sr - 1) >= eps
         x = NaN;
         step = -1;
-        disp('Matricea nu converge');
+        disp('Matrix does not converge');
         return;
     endif
-    
-    % for the function to work make sure
-    % that b and x0 are column vectors
     
     n = length(b); 
     x = x0;
@@ -25,27 +22,21 @@ function [x step] = Jacobi(A, b, x0, tol, max_iter)
     for step = 1 : max_iter
         % iterate through every x(i)
         for i = 1 : n
-            % val_sum represents the sum of the 
-            % Jacobi algortihm and A(i, i) * x0(i) 
-            % for which we calculate
-            % val_x so that we can 
-            % substract it from val_sum
-            
+            % val_sum represents the sum of the Jacobi algortihm and A(i, i) * x0(i) 
+            % for which we calculate val_x so that we can  substract it from val_sum
             val_sum = A(i, :) * x0(:);
             val_x = A(i, i) * x0(i);
+            
             x(i) = b(i) - (val_sum - val_x);
             x(i) = x(i) / A(i, i);
             
         endfor
-        % when the new values get close 
-        % enough to the last values 
-        % regarding the imposed tolerance 
-        % "tol", we reached the solution
+        % when the new values get close enough to the last values 
+        % regarding the imposed tolerance "tol", we reached the solution
         if norm(x - x0) < tol
             break;
         endif
-        % update the last computed 
-        % values with the new values
+        % update the last computed values with the new values
         x0 = x;
     endfor
 endfunction 
