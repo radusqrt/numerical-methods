04ff386553e0fc712047611eaeda67b62b3fde98
Serban Dragos <dragos.marian1998@gmail.com>
2018-03-22 21:06:54 +0200

    Add files via upload

--- MN/Algoritmi/Metode-Iterative/Gauss-Seidel/GaussSeidel.m
+++ MN/Algoritmi/Metode-Iterative/Gauss-Seidel/GaussSeidel.m
@@ -7,17 +7,14 @@ function [x step] = GaussSeidel(A, b, x0, tol, max_iter)
     Ggs = inv(N) * P;
     % calculate the spectral radius of Ggs
     sr = max(abs(eig(Ggs)));
-    % check if the algorithm converges
     
+    % check if the algorithm converges
     if  (sr - 1) >= eps
         x = NaN;
         step = -1;
-        disp('Matricea nu converge');
+        disp('Matrix does not converge');
         return;
     endif
-    
-    % for the function to work make sure
-    % that b and x0 are column vectors
     
     n = length(b);
     x = x0;
@@ -25,25 +22,21 @@ function [x step] = GaussSeidel(A, b, x0, tol, max_iter)
     for step = 1 : max_iter
         % iterate through every x(i)
         for i = 1 : n
-            % new_values_sum represents the first sum
-            % of the Gauss-Seidel algorithm
+            % new_values_sum represents the first sum of the Gauss-Seidel algorithm
             new_values_sum = A(i, 1 : i - 1) * x(1 : i - 1);
             
-            % old_values_sum represents the second sum
-            % of the Gauss-Seidel algorithm
+            % old_values_sum represents the second sum of the Gauss-Seidel algorithm
             old_values_sum = A(i, i + 1 : n) * x0(i + 1 : n);
             
-            x(i) = (b(i) - (old_values_sum + new_values_sum)) / A(i, i);
+            x(i) = b(i) - (old_values_sum + new_values_sum);
+            x(i) = x(i) / A(i, i);
         endfor
-        % when the new values get close 
-        % enough to the last values 
-        % regarding the imposed tolerance 
-        % "tol", we reached the solution
+        % when the new values get close enough to the last values 
+        % regarding the imposed tolerance "tol", we reached the solution
         if norm(x - x0) < tol
             break;
         endif
-        %   update the last computed 
-        %   values with the new values
+        % update the last computed values with the new values
         x0 = x;
     endfor
 endfunction 
