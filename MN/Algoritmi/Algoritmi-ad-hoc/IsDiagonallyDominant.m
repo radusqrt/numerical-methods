49871d453d713ceefa76e5980e8b7802ecf3166b
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-03-23 15:47:05 +0200

    Fix tabs

--- MN/Algoritmi/Algoritmi-ad-hoc/IsDiagonallyDominant.m
+++ MN/Algoritmi/Algoritmi-ad-hoc/IsDiagonallyDominant.m
@@ -1,17 +1,17 @@
 % checks if a matrix A is diagonally dominant 
 function is_diag_dom = IsDiagonallyDominant(A)
     [n n] = size(A);
-	% I assume it is diagonally dominant
-	is_diag_dom = 1;
+    % I assume it is diagonally dominant
+    is_diag_dom = 1;
 
-	for i = 1 : n
-		% calculate the sum of absolute value of 
-		%the elements on line i, except A(i, i)
-		sum_of_line = sum(abs(A(i, :))) - abs(A(i, i));
+    for i = 1 : n
+        % calculate the sum of absolute value of 
+        %the elements on line i, except A(i, i)
+        sum_of_line = sum(abs(A(i, :))) - abs(A(i, i));
 
-		if abs(A(i, i)) <= sum_of_line
-			is_diag_dom = 0;
-			break;
-		endif
-	endfor
+        if abs(A(i, i)) <= sum_of_line
+            is_diag_dom = 0;
+            break;
+        endif
+    endfor
 endfunction
