3f4867e0e49b38b19d99b128e6dc4d904202c051
roxanaN <roxxana.roxx7@gmail.com>
2018-03-26 21:40:58 +0300

    Metoda secantei

--- MN/Algoritmi/Ecuatii-Neliniare/Secant_Method.m
+++ MN/Algoritmi/Ecuatii-Neliniare/Secant_Method.m
@@ -1,36 +1,42 @@
-function [x] = Secant_Method(xn_2, xn_1, tol, p, max_iter)
+function [x, steps] = Secant_Method(f, x0, x1, tol, max_iter)
     % Solves f(x) = 0 by doing max_iter steps of the secant method
-    % the secant method requires two initial values, xn_1 and xn_2
+    % the secant method requires two initial values, x0 and x1
     % which should ideally be chosen close to the root
-    % y = polyval(p, x) returns the value of a polynomial evaluated at x
+    % y = feval(f, x)  evaluates a function using its name or its handle
+    %     and using the input arguments
+    steps = 0;
     for i = 1 : max_iter
-        % function values
-        f_xn_2 = polyval(p, xn_2);
-        f_xn_1 = polyval(p, xn_1);
+        steps = steps + 1;
+
+        % function values: f(x0) and f(x1)
+        f0 = feval(f, x0);
+        f1 = feval(f, x1);
 
         % the root of function f is approximated using the formula 
-        xn = xn_2 - f_xn_2 * (xn_2 - xn_1) / (f_xn_2 - f_xn_1);
+        xi = x1 - f1 * (x1 - x0) / (f1 - f0);
 
-        % calculate f(xn)
-        f_xn = polyval(p, xn);
+        % calculate f(xi)
+        fxi = feval(f, xi);
 
-        % xn is the solution
-        if f_xn == 0
-            x = xn;
+        % xi is the solution
+        if abs(fxi) < eps
+            x = xi;
             return;
         endif
 
         % calculate eps
-        eps = abs((xn - xn_2) / xn);
+        epsilon = abs((xi - x1) / xi);
 
         % is achieved a sufficient degree of accuracy eps
-        if eps < tol
-            x = xn;
+        if epsilon < tol
+            x = xi;
             return;
         endif
 
         % the new values for the next step
-        xn_1 = xn_2;
-        xn_2 = xn;
+        x0 = x1;
+        x1 = xi;
     endfor
+
+    disp('Maximum number of iterations reached');
 endfunction
