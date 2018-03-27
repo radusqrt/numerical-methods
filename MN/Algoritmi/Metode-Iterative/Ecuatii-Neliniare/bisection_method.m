c56c411163e87af9697e98816a5b3043ed973145
missCleare <37804247+missCleare@users.noreply.github.com>
2018-03-27 23:28:52 +0300

    BisectionMethod.m

--- MN/Algoritmi/Metode-Iterative/Ecuatii-Neliniare/bisection_method.m
+++ MN/Algoritmi/Metode-Iterative/Ecuatii-Neliniare/bisection_method.m
@@ -1,33 +1,32 @@
-function [m] = bisection_method(f, tol, a, b)
-	while 1
-		m = (a + b) / 2;
-		fact = polyval(f, a) * polyval(f, m);
+function [x i] = BisectionMethod(f, a, b, tol, max_iter)
+	for i = 1 : max_iter
+		x = (a + b) / 2;
+		fact = feval(f, a) * feval(f, x);
 
 		% if the product is negative, the root is in 
 		% between a and m
 		if fact < 0
-			b = m;
-		endif
+			b = x;
 
 		% if the product is negative, the root is in 
-		% between a and m
-		if fact > 0
-			a = m;
-		endif
+		% between b and x
+		elseif fact > 0
+			a = x;
 
 		% if the product is null, the root is found
-		if fact == 0
+		else
 			return;
 		endif
 
 		% if the root is unknown, the new_m is calculated
-		new_m = (a + b) / 2;
+		new_x = (a + b) / 2;
 
-		% if the error is lower than tolerance, m becomes the solutions
-		err = abs ((new_m - m) / new_m);
+		% if the relative absolute error is lower than tolerance,
+		% bisection method reached convergence limit
+		err = abs((new_x - x) / new_x);
 		if err < tol
-			m =  new_m;
+			x =  new_x;
 			return;
 		endif
-	endwhile
+	endfor
 endfunction
