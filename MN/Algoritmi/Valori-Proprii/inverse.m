3bf98ee1ae558ed54ffe53776c6b25511139015e
Andrei Dinu <dinuaadrian@gmail.com>
2018-04-05 23:54:52 +0300

    Update inverse.m

--- MN/Algoritmi/Valori-Proprii/inverse.m
+++ MN/Algoritmi/Valori-Proprii/inverse.m
@@ -1,9 +1,9 @@
 function [Ai] = inverse(A, miu, I)
-	% example of inverting using LU here:
-	% https://www.youtube.com/watch?v=dza5JTvMpzk
-	[L U] = lu(A - miu * I);
+    % example of inverting using LU here:
+    % https://www.youtube.com/watch?v=dza5JTvMpzk
+    [L U] = lu(A - miu * I);
 
-	z1 = L \ [1; 0; 0];
+    z1 = L \ [1; 0; 0];
     col1 = U \ z1;
     z2 = L \ [0; 1; 0];
     col2 = U \ z2;
