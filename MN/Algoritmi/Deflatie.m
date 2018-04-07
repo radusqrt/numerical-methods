11b9ed5eae7e8cc76d0d4cfad258bc95f60985a2
Valentin Ionita <33692650+vanntile@users.noreply.github.com>
2018-04-07 15:57:49 +0300

    Formatare

--- MN/Algoritmi/Deflatie.m
+++ MN/Algoritmi/Deflatie.m
@@ -1,20 +1,21 @@
+% [USES] PutereDirecta(), PutereInversa()
 function [L, V] = Deflatie(A, max_iter, tol)
-	[n n] = size(A);
-	L = zeros(1, n);
-	V = zeros(n);
+  [n n] = size(A);
+  L = zeros(1, n);
+  V = zeros(n);
   C = A;
 
-	for k = 1 : n - 1
+  for k = 1 : n - 1
   % getting the current eigenvalue
     [lam y nr] = PutereDirecta(A, max_iter, tol);
     L(k) = lam;
     V(:, k) = y;
     
     % calculating the next matrix
-    x = A(k, :)'/ (lam * y(k));
+    x = A(k, :)' / (lam * y(k));
     B = A - lam * y * x';
     A = B;
-	endfor
+  endfor
   
   % getting the last eigenvalue
   [lam y nr] = PutereDirecta(A, max_iter, tol);
