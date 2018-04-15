f0eccc0aace809c414020f830fdce2988245cfc1
andreidinu <dinuaadrian@gmail.com>
2018-04-15 13:01:15 -0700

    Inverse Power Method

--- Eigenvalues/InversePowerMethod.m
+++ Eigenvalues/InversePowerMethod.m
@@ -1,6 +1,8 @@
-% [USES] Ad-hoc/SST.m
-% [USES] Gauss/G/G.m
+% [USES] Ad-hoc/SIT
+% [USES] Ad-hoc/SST
+% [USES] LU/Doolittle
 function [lambda, y, step] = InversePowerMethod(A, tol, max_iter, miu)
+    % Check if it's a square matrix, otherwise there won't be any eigenvalues
     [n m] = size(A);
     if n ~= m
         disp ('Not square matrix')
@@ -12,21 +14,21 @@ function [lambda, y, step] = InversePowerMethod(A, tol, max_iter, miu)
     lambda = inf;
 
     for step = 1 : max_iter
-        % Solving (A - miu * I) * z = y system using Gauss
-        z = G(A - miu * I, y); 
+        % Solving (A - miu * I) * z = y system using Doolittle LU decomposition 
+        [L U z] = Doolittle(A - miu * I, y);
     	y = z / norm(z);
 
         % Update the previous value with the newest one
     	lambda_old = lambda;
         lambda = y' * A * y;
-        
+
         % When the new values get close enough to the last values
         % regarding the imposed tolerance "tol", we reached the solution
     	if abs((lambda - lambda_old) / lambda) < tol
     		return;
     	endif
 
-    	% Rayleigh quotient iteration
+    	% Rayleigh quotient iteration in order to increase the convergence speed
     	miu = lambda;
     endfor
 endfunction
