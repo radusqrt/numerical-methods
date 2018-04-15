76cc14048b1c93b4c743624a92e1c2e792a64151
Carmen Popa <carmen.popa@mready.net>
2018-04-15 21:37:07 +0300

    addes success; stopped switching lines

--- Ad-hoc/GaussJordan.m
+++ Ad-hoc/GaussJordan.m
@@ -1,37 +1,31 @@
-%calculates the reverse of matrix A
-function B = GaussJordan(A)
+% computes reverse of matrix A
+function [B success] = GaussJordan(A)
     [n n] = size(A);
     B = eye(n);
+    % I assume A has an inverse matrix
+    success = 1;
 
     if det(A) == 0
         %A cannot be reversed
         B = NaN;
+        success = 0;
         return;
     endif 
     
-    %merge thw two matrices
+    % merge the two matrices
     Ae = [A B];
     for i = 1 : n
         if Ae(i, i) == 0
-            %change lines
-            for k = i : n
-                if Ae(k, k) ~= 0
-                    %change line i with line k in matrix A
-                    aux = Ae(i, :);
-                    Ae(i, :) = Ae(k, :);
-                    Ae(k, :) = aux;
-
-                    %only one interchange is needed
-                    break;
-                endif
-            endfor
+            B = NaN;
+            success = 0;
+            return;
         endif
 
-        %calculate the pivot
+        % calculate the pivot
         Ae(i, :) = Ae(i, :) / Ae(i, i);
 
-        %form zeros above and under the main diagonal in A
-        %calculate the inverse in B
+        % form zeros above and under the main diagonal in A
+        % calculate the inverse in B
         for j = 1 : n
             if i ~= j
                 Ae(j, :) = Ae(j, :) - Ae(i, :) * Ae(j, i);
@@ -39,6 +33,6 @@ function B = GaussJordan(A)
         endfor
     endfor
     
-    %extract matrix B
+    % extract matrix B
     B = Ae(:, n + 1 : 2 * n);
 endfunction
