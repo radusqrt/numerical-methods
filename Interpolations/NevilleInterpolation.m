2eabe9c5409e4e639f8d318f0d17093e59e4152c
Radu Stochitoiu <radu.stochitoiu@gmail.com>
2018-04-22 13:15:11 +0300

    Add Neville Interpolation

---
+++ Interpolations/NevilleInterpolation.m
@@ -0,0 +1,31 @@
+% [USES] Interpolations/NevillePoint
+% Function that calculates the Neville interpolation polynomial for a vector
+% of points (a, b) with a:xs, b:ys in "elements" points between the first of xs
+% and the last of xs; it also plots the interpolation
+function [xss yss] = NevilleInterpolation(xs, ys, elements)
+    n = length(xs);
+    % if "elements" is given, use it; otherwise, use the default value of
+    % linspace (usually 100)
+    if exist("elements", "var") == 1
+        xss = linspace(xs(1), xs(n), elements);
+    else
+        xss = linspace(xs(1), xs(n));
+    endif
+
+    % create an empty vector to store the values of the interpolation in the
+    % xss' points
+    yss = [];
+    for i = 1:length(xss)
+        % Calculate the approximation of the function in xss(i)
+        yss = [yss NevillePoint(0, n - 1, xss(i), xs, ys)];
+    endfor
+
+    % open a new figure to merge two plots
+    figure
+    % plot the interpolated function
+    plot(xss, yss, "linewidth", 3)
+    % keep the chart in the figure to plot something else on top of it
+    hold on
+    % plot the initial points as circles
+    plot(xs, ys, 'o', 'linewidth', 3)
+endfunction
---
+++ Interpolations/tests/test_NevilleInterpolation.m
@@ -0,0 +1,5 @@
+% [USES] Interpolations/NevilleInterpolation
+
+xs = [1, 2, 3, 4, 5, 6, 7, 8];
+ys = sin(xs) + 2;
+[xss yss] = NevilleInterpolation(xs, ys, 100);
