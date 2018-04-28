e59c7cf58a2ddb292c5a01a60dc8a44afe8b4e2f
Valentin Ionita <33692650+vanntile@users.noreply.github.com>
2018-04-28 10:15:35 +0300

    Create test_DirectFFTInterpolation.m

---
+++ Interpolations/tests/test_DirectFFTInterpolation.m
@@ -0,0 +1,17 @@
+% [USES] Interpolations/DirectFFTInterpolation
+n = 128;
+m = 20000;
+% creating the input n points
+x = linspace(-pi, pi, n);
+% the function we're trying to interpolate between [-0.9*pi; 0.9*pi]
+y = x.^3+2.*(x.^2)-5.*x;
+% interpolating in m points
+z = linspace(-pi*0.9, pi*0.9, m);
+% the actual function values for comparison
+z2 = z.^3+2.*(z.^2)-5.*z;
+
+% applying the Direct Fast Fourier Transform
+S = DirectFFTInterpolation(x, y, z);
+
+% plot the two results for graphic comparison
+plot(z, S, z, z2);
